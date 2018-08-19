c71f1e23f62ffcfbe804395146431d69e1f23b53
joe mckinney <mckinney.a.joe@gmail.com>
2018-08-18 18:43:07 -0700

    add content to galleries page.

--- src/components/Galleries.jsx
+++ src/components/Galleries.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import cloudyDay from './../assets/images/clouds1.JPG';
+import shake from './../assets/images/shake23.JPG';
+import redcar from './../assets/images/redcar.JPG';
+import img from './../assets/images/08.JPG';
 
 
 function Galleries() {
   return(
-    <p>This will be a hot ass gallery.</p>
+    <div>
+      <div className="gallery-thumbs">
+        <img src={cloudyDay} />
+        <p>Landscapes</p>
+      </div>
+      <div className="gallery-thumbs">
+        <img src={img} />
+        <p>Cityscapes</p>
+      </div>
+      <div className="gallery-thumbs">
+        <img src={shake} />
+        <p>People/Weddings/Family</p>
+      </div>
+      <div className="gallery-thumbs">
+        <img src={redcar} />
+        <p>Arts/Funs</p>
+      </div>
+
+    </div>
   );
 }
 
