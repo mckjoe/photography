97234f3f80859da4d09d31b7ad92503beaec9e3c
joe mckinney <mckinney.a.joe@gmail.com>
2018-08-18 21:51:33 -0700

    remove class from div in app.js.

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ import Footer from './Footer';
 class App extends Component {
   render() {
     return (
-      <div className='test'>
+      <div>
         <Header />
         <hr />
         <Switch>
